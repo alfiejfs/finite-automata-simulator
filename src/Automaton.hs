0a9cd3b446c73b98c7e31eb74972a3c962593a11
alfiejfs <alfie@alfiesmith.net>
2024-01-05 19:00:05 +0000

    remove silly state monad

--- src/Automaton.hs
+++ src/Automaton.hs
@@ -1,16 +1,18 @@
-module Automaton (Character, Alphabet, State(..), Automaton(..)) where
+module Automaton (Character, Alphabet, Input, State(..), ComputationStep(..), Automaton(..)) where
 
-
+-- Represents a character that can be taken as input by an Automaton
 type Character = String
+
+type Input = [Character]
 
 -- Represents the alphabet over which an automaton can operate
 type Alphabet = [Character]
-
-
 
 newtype State = State { 
     getIdentifier :: String
 } deriving (Show, Eq)
 
+data ComputationStep = Error String | Termination Bool | ComputationStep State
+
 class Automaton a where
-    runAutomaton :: a -> String -> Bool
+    runAutomaton :: a -> Input -> ComputationStep
--- src/DFA.hs
+++ src/DFA.hs
@@ -1,12 +1,12 @@
 module DFA (DFA(..)) where
 
 import Automaton
-import qualified Control.Monad.State as S
 
 -- A deterministic finite automaton (DFA) is a 5-tuple {
 --  The states Q
 --  The alphabet \Sigmaa
 --  The transition function \delta
+--      Returns Nothing if and only if the character was not in the Alphabet
 --  The start state q_0
 --  The accept states F
 -- }
@@ -18,27 +18,17 @@ data DFA = DFA {
   , acceptStates :: [State]
 }
 
--- Represents the final result of a DFA computation.
--- Left is an error message for a computation that failed to calculate.
--- Right is whether the computation was an accept or a reject on the DFA 
-type ComputationResult = Either String Bool 
+instance Automaton DFA where
+    runAutomaton dfa i = case final of
+                            ComputationStep s -> Termination $ s `elem` acceptStates dfa
+                            x -> x
+                       where
+                            foldFunc c (ComputationStep s) = stepDFA dfa s c
+                            foldFunc _ x = x
+                            final = foldr foldFunc (ComputationStep $ startState dfa) i
 
--- Represents a step after a computation in a DFA
--- ComputationResult is when the computation is finished
--- State is when the computation has more to do
-type ComputationStep = Either ComputationResult State
-
--- Represents a computation over an input string
-type DFAComputation = S.State [Character]
-
-stepDFA :: DFA -> State -> DFAComputation ComputationStep
-stepDFA dfa state = do
-    inputs <- S.get 
-  
-    let nextState = transition dfa state $ head inputs
-    
-    case nextState of 
-        Nothing -> return $ Left $ Right $ state `elem` acceptStates dfa
-        Just s -> do
-            S.put $ tail inputs
-            return $ Right s
+stepDFA :: DFA -> State -> Character -> ComputationStep
+stepDFA dfa s c = let nextState = transition dfa s c
+                  in case nextState of
+                    Nothing -> Error $ "Character " ++ c ++ " not in alphabet."
+                    Just n  -> ComputationStep n
